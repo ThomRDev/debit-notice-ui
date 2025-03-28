904c09c8e2cd0ab3424c20352dff5c94156cc931
ThomRdev <thom.maurick.r@gmail.com>
2025-03-28 13:35:21 -0500

    add

---
+++ src/hooks/useDebitNotices.ts
@@ -0,0 +1,27 @@
+import { useQuery } from "@tanstack/react-query";
+import { SearchParams, useSearchStore } from "../store/debitNotices.store";
+import DebitServiceApi from "../config/api";
+
+const buildQueryParams = (params: SearchParams): string => {
+  const queryParams = new URLSearchParams(
+    Object.entries(params).map(([key, value]) => [key, String(value)])
+  );
+  return queryParams.toString();
+};
+
+export const useDebitNotices = () => {
+  const { searchParams } = useSearchStore();
+  const queryParams = buildQueryParams(searchParams);
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["debit-notice", queryParams],
+    queryFn: async () => {
+      const response = await DebitServiceApi.get(
+        `/debit-notice?${queryParams}`
+      );
+      return response.data;
+    },
+    staleTime: 10000,
+  });
+
+  return { data, isLoading, error };
+};
---
+++ src/store/debitNotices.store.ts
@@ -0,0 +1,66 @@
+import { create } from "zustand";
+import { devtools, persist } from "zustand/middleware";
+
+export interface SearchParams {
+  numero_aviso: string;
+  estado: string;
+  numero_sap: string;
+  usuario_creador: string;
+  fecha_desde: string;
+  fecha_hasta: string;
+  cliente: string;
+  moneda: string;
+  importe_desde: string;
+  importe_hasta: string;
+}
+
+export const useSearchStore = create(
+  devtools(
+    persist<{
+      searchParams: SearchParams;
+      updateSearchParams: (params: Partial<SearchParams>) => void;
+      resetSearchParams: () => void;
+    }>(
+      (set) => ({
+        searchParams: {
+          numero_aviso: "",
+          estado: "Todos",
+          numero_sap: "",
+          usuario_creador: "",
+          fecha_desde: "",
+          fecha_hasta: "",
+          moneda: "Todas",
+          importe_desde: "0.00",
+          importe_hasta: "0.00",
+          cliente: "",
+        },
+
+        updateSearchParams: (params) =>
+          set((state) => ({
+            searchParams: { ...state.searchParams, ...params },
+          })),
+
+        resetSearchParams: () => {
+          set({
+            searchParams: {
+              numero_aviso: "",
+              estado: "Todos",
+              numero_sap: "",
+              usuario_creador: "",
+              fecha_desde: "",
+              fecha_hasta: "",
+              moneda: "Todas",
+              importe_desde: "0.00",
+              importe_hasta: "0.00",
+              cliente: "",
+            },
+          });
+          localStorage.removeItem("search-params-storage");
+        },
+      }),
+      {
+        name: "search-params-storage",
+      }
+    )
+  )
+);
