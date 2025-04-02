4aa2eb79558a9ae18e76930855ec710bfead0799
ThomRdev <thom.maurick.r@gmail.com>
2025-04-02 13:41:18 -0500

    remove duplicate client

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AppRouter } from "./router/AppRouter";
 
 function App() {
-  const queryClient = new QueryClient();
+  // const queryClient = new QueryClient();
   return (
     <>
-    <QueryClientProvider client={queryClient}>
+      {/* <QueryClientProvider client={queryClient}> */}
       <AppRouter />
-    </QueryClientProvider>
-      
+      {/* </QueryClientProvider> */}
     </>
   );
 }
